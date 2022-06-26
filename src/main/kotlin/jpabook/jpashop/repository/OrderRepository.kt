6ec9bdd0e6809378679d19a7f0c393dfff060b9f
goyangii <cottonblue713@gmail.com>
2022-06-26 18:35:27 +0900

    feat: add OrderSearch

--- src/main/kotlin/jpabook/jpashop/repository/OrderRepository.kt
+++ src/main/kotlin/jpabook/jpashop/repository/OrderRepository.kt
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.Order
 import org.springframework.stereotype.Repository
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
+import jpabook.jpashop.repository.OrderSearch
 
 @Repository
 class OrderRepository {
@@ -19,5 +20,14 @@ class OrderRepository {
         return em.find(Order::class.java, id)
     }
 
-//    fun findAll(orderSearch: OrderSearch): List<Order> {}
+    fun findAll(orderSearch: OrderSearch): List<Order> {
+        // XXX: 나중에 queryDSL 을 사용하도록 수정
+        return em.createQuery("select o from Order o join o.member m " +
+                "where o.status = : status " +
+                "and m.name like :name", Order::class.java)
+            .setParameter("status", orderSearch.orderStatus)
+            .setParameter("name", orderSearch.memberName)
+            .setMaxResults(1000)
+            .resultList
+    }
 }
