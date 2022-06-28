f04e231adafc6f85416ea058b8f20cbe561dff01
goyangii <cottonblue713@gmail.com>
2022-06-28 23:55:17 +0900

    feat: add an item update function

--- src/main/kotlin/jpabook/jpashop/controller/ItemController.kt
+++ src/main/kotlin/jpabook/jpashop/controller/ItemController.kt
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
@@ -32,5 +34,21 @@ class ItemController (
         val items = itemService.findItems()
         model.addAttribute("items", items)
         return "items/itemList"
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    fun updateItemForm(@PathVariable("itemId") itemId: Long, model: Model): String {
+        val item: Book = itemService.findOne(itemId) as Book
+        val form = BookForm(id = item.id, name = item.name, price = item.price, stockQuantity = item.stockQuantity, author = item.author, isbn = item.isbn)
+
+        model.addAttribute("form", form)
+        return "items/updateItemForm"
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    fun updateItem(@PathVariable("itemId") itemId: Long, @ModelAttribute("form") form: BookForm): String {
+        val book = Book(id = form.id, name = form.name!!, price = form.price!!, stockQuantity = form.stockQuantity!!, author = form.author, isbn = form.isbn)
+        itemService.saveItem(book)
+        return "redirect:/items"
     }
 }
