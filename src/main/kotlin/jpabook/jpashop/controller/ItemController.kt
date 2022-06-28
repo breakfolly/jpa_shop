06981000f6d5e77fb125303c774e9c7eb51b3504
goyangii <cottonblue713@gmail.com>
2022-06-28 23:24:23 +0900

    feat: add item controller and form

---
+++ src/main/kotlin/jpabook/jpashop/controller/ItemController.kt
@@ -0,0 +1,29 @@
+package jpabook.jpashop.controller
+
+import jpabook.jpashop.domain.Item.Book
+import jpabook.jpashop.service.ItemService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller
+class ItemController (
+    @Autowired
+    val itemService: ItemService
+    ){
+
+    @GetMapping("/items/new")
+    fun createForm(model: Model): String {
+        model.addAttribute("form", BookForm())
+        return "items/createItemForm"
+    }
+
+    @PostMapping("/items/new")
+    fun create(form: BookForm): String {
+        val book = Book(name = form.name!!, price = form.price!!, stockQuantity = form.stockQuantity!!, author = form.author, isbn = form.isbn)
+        itemService.saveItem(book)
+        return "redirect:/items"
+    }
+}
